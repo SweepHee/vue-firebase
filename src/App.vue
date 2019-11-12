7239074006140172acf834f2c2a166e82a2aa36a
Sweep <jeonsh1991@gmail.com>
2019-11-13 01:59:08 +0900

    navigation group

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-toolbar flat color='transparent'>
+        <v-toolbar-title>Account</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+        >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item
+          v-for="subItem in item.subItems"
+          :key="subItem.title"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
   <v-card
     color="grey lighten-4"
     flat
     tile
   >
-    <v-toolbar height="64px" dense flat color="gray">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar height="64px" dense color="green lighten-3">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Title</v-toolbar-title>
 
@@ -28,7 +57,7 @@
 
      <v-content>
         <router-view/>
-      </v-content>
+    </v-content>
   </v-app>
 </template>
 
@@ -39,6 +68,45 @@ export default {
 
   components: {
 
+  },
+  data () {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-alert',
+          title: 'home',
+          active: true,
+          subItems: [
+            {
+              title: 'dashboard',
+              to: '/'
+            },
+            {
+              title: 'About2',
+              to: '/about'
+            }
+          ]
+        },
+        {
+          icon: 'mdi-alert-box',
+          title: 'Lectures',
+          active: false,
+          subItems: [
+            {
+              title: 'card',
+              to: '/lectures/card'
+            },
+            {
+              title: 'layout',
+              to: '/lectures/layout'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
   }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,7 @@
 <template>
- <v-list>
-   <v-list-tile two-line v-for="item in items" :key="item.title">
-     <v-list-avatar>
-       <v-icon>
-         {{item.icon}}
-       </v-icon>
-     </v-list-avatar>
-     <v-list-tile-content>
-       <v-list-tile-title>
-         {{item.title}}
-       </v-list-tile-title>
-     </v-list-tile-content>
-     <!-- <v-list-tile-action>
-       <v-btn icon ripple>
-         <v-icon color="grey lighten-1">mdi-alert-box</v-icon>
-       </v-btn>
-     </v-list-tile-action> -->
-   </v-list-tile>
- </v-list>
+ <div>
+   about!
+ </div>
 </template>
 
 <script>
